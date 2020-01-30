669f0982c04c5f09d129d9e90848732680cdd2e9
Matthew Boswell <mboswell2@gmail.com>
2020-01-30 04:52:38 -0500

    wordpress comment

---
+++ plugdation.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name:  Plugdation
+Description:  A starting foundation for your wordpress plugin.
+Plugin URI:   https://WEBSITE
+Author:       Matthew Boswell
+Version:      1.0
+Text Domain:  plugdation
+Domain Path:  /languages
+License:      MIT License
+License URI:  https://opensource.org/licenses/MIT
+*/
+
+
+// disable direct file access
+if ( ! defined( 'ABSPATH' ) ) {
+
+    exit;
+
+}
+
+/**
+ * Class Plugdation
+ */
+class Plugdation {
+
+    /**
+     * Plugdation constructor.
+     */
+    public function __construct()
+    {
+    }
+}
