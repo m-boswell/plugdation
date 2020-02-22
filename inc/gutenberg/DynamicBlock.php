9c138d8687bb4f4fd3bae1aeaead9a999756c557
Matthew Boswell <mboswell2@gmail.com>
2020-02-22 00:21:08 -0500

    Lessening boilerplate of dynamic block with arg unpacking

--- inc/gutenberg/DynamicBlock.php
+++ inc/gutenberg/DynamicBlock.php
@@ -8,11 +8,17 @@ use Plugdation\Plugdation\gutenberg\Block;
 
 abstract class DynamicBlock extends Block
 {
-    public function __construct(string $name)
-    {
-        parent::__construct($name);
+    /**
+     * DynamicBlock constructor.
+     * @inheritDoc
+     */
+    public function __construct(...$args) {
+        parent::__construct(...$args);
+        $this->setAttributes();
         $this->render_callback = [$this, 'dynamic_render_callback'];
     }
+
+    abstract protected function setAttributes();
 
     abstract public function dynamic_render_callback($attributes, $content);
 
