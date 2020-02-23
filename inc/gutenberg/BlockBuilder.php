1b12327a594eb1a696e7d55983da3ea5ae35dbe9
Matthew Boswell <mboswell2@gmail.com>
2020-02-23 07:52:52 -0500

    BlockBuilder glob
    glob is a helper method for building a block with its resources inside a flat single directory.

--- inc/gutenberg/BlockBuilder.php
+++ inc/gutenberg/BlockBuilder.php
@@ -4,6 +4,10 @@
 namespace Plugdation\Plugdation\gutenberg;
 
 
+use Plugdation\Plugdation\assets\ScriptAsset;
+use Plugdation\Plugdation\assets\StyleAsset;
+use Plugdation\Plugdation\hooks\RegisterHooks;
+
 class BlockBuilder
 {
     /** @var string */
@@ -102,15 +106,54 @@ class BlockBuilder
      */
     public function build()
     {
-        return new Block( 
-            $this->name, 
-            $this->editor_script, 
-            $this->script, 
-            $this->editor_style, 
-            $this->style, 
-            $this->render_callback, 
+        return new Block(
+            $this->name,
+            $this->editor_script,
+            $this->script,
+            $this->editor_style,
+            $this->style,
+            $this->render_callback,
             $this->attributes
         );
+    }
+
+    public static function glob($namespace, $block_name, $dir, $config = array(
+        'editor_script' => true,
+        'editor_style' => true,
+        'script' => true,
+        'style' => true
+    )) {
+        $Register_Hooks = new RegisterHooks();
+
+        if ($config['editor_script']) {
+            $Script_Asset = new ScriptAsset("$namespace-$block_name-block",  "$dir/index.js");
+            $Register_Hooks->register($Script_Asset);
+        }
+
+        if ($config['editor_style']) {
+            $Editor_Asset = new StyleAsset("$namespace-$block_name-block-editor", "$dir/editor.css");
+            $Register_Hooks->register($Editor_Asset);
+        }
+
+        if ($config['script']) {
+            $Script_Front_Asset = new ScriptAsset("$namespace-$block_name-block-frontend", "$dir/front.js");
+            $Register_Hooks->register($Script_Front_Asset);
+        }
+
+        if ($config['style']) {
+            $Style_Asset = new StyleAsset("$namespace-$block_name-block-frontend", "$dir/style.css");
+            $Register_Hooks->register($Style_Asset);
+        }
+
+        $Block = ( new BlockBuilder() )
+            ->setName("$namespace/$block_name")
+            ->setEditorScript($Script_Asset->getHandle())
+            ->setEditorStyle($Editor_Asset->getHandle())
+            ->setScript($Script_Front_Asset->getHandle())
+            ->setStyle($Style_Asset->getHandle())
+            ->build();
+
+        return $Block;
     }
 
 }
