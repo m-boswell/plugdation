5289ec862cafb1853eb842a7cb53348c7778aa71
Matthew Boswell <mboswell2@gmail.com>
2020-02-23 08:54:38 -0500

    Proof of Render

---
+++ tests/templating/index.php
@@ -0,0 +1,6 @@
+<?php
+
+use Plugdation\tests\templating\proof\HelloWorld;
+
+$Hello_World = new HelloWorld();
+$Hello_World->render();
---
+++ tests/templating/proof/HelloWorld.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Plugdation\tests\templating\proof;
+
+
+use Plugdation\Plugdation\templating\Render;
+
+class HelloWorld extends Render
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getMarkup(): string
+    {
+        return '<p>Testing Render with a concrete class. Check check, 1, 2, 3...check check...CHEEEEEEECCCCCKKKKKKK.</p>';
+    }
+}
