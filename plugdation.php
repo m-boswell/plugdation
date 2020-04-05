7b75752d914b9b24ad1116edc8ef1047924b35cd
Matthew Boswell <mboswell2@gmail.com>
2020-04-05 09:33:30 -0400

    webpack build

--- plugdation.php
+++ plugdation.php
@@ -11,6 +11,11 @@ License:      MIT License
 License URI:  https://opensource.org/licenses/MIT
 */
 
+use Plugdation\Plugdation\assets\ScriptAsset;
+use Plugdation\Plugdation\assets\StyleAsset;
+use Plugdation\Plugdation\hooks\RegisterHooks;
+
+
 // disable direct file access
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
@@ -19,7 +24,29 @@ require_once __DIR__ . './inc/constants.php';                   // plugin consta
 include_once __DIR__ . './app/index.php';                       // main plugin folder.
 
 
+$Register_Hooks = new RegisterHooks();
+$Admin_Asset = new ScriptAsset('plugdation-admin', 'src/admin/index.js');
+//$Admin_Asset = new StyleAsset('plugdation-admin', 'src/admin/style.css');
+$Register_Hooks->register($Admin_Asset);
+//$Register_Hooks->register($Admin_Asset);
 
+$Blocks_Asset = new ScriptAsset('plugdation-blocks', 'src/blocks/index.js');
+$Register_Hooks->register($Blocks_Asset);
+
+$Frontend_Asset = new ScriptAsset('plugdation-frontend', 'src/frontend/index.js');
+$Register_Hooks->register($Frontend_Asset);
+
+function plugdation_enqueue_assets() {
+    wp_enqueue_script('plugdation-admin');
+//    wp_enqueue_script('plugdation-blocks');
+    wp_enqueue_script('plugdation-frontend');
+}
+add_action( 'wp_enqueue_scripts', 'plugdation_enqueue_assets' );
+
+function plugdation_enqueue_admin_assets(  ) {
+    wp_enqueue_script('plugdation-admin');
+}
+add_action( 'admin_enqueue_scripts', 'plugdation_enqueue_admin_assets' );
 
 
 // Uncomment to test proof of concepts
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const path = require("path");
+
+module.exports = {
+    ...defaultConfig,
+    entry: {
+        index: path.resolve( process.cwd(), 'src/blocks', 'index.js' ),
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve( process.cwd(), 'build' ),
+    },
+    module: {
+        ...defaultConfig.module,
+        rules: [
+            ...defaultConfig.module.rules,
+            {
+                test: /\.css$/,
+                use: ["css-loader", "style-loader"],
+            }
+        ]
+    }
+};
